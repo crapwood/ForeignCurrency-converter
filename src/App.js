e909a0e9cb419ae50bfa856b49221d3824d8e56d
james <jjbe.93@gmail.com>
2020-03-09 04:29:29 +0200

    finish logic

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ function App() {
   const [result, setResult] = useState(0);
   const [fromVal, setFromVal] = useState(0);
   const [toVal, setToVal] = useState(0);
+  const [fromCurr, setFromCurr] = useState("USD");
+  const [toCurr, setToCurr] = useState("USD");
 
   useEffect(() => {
     setResult(fromVal * toVal);
   }, [toVal, fromVal]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let url = `https://api.exchangeratesapi.io/latest?base=${fromCurr}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setToVal(parseFloat(data.rates[toCurr]));
+    };
+    fetchData();
+  }, [fromCurr, toCurr]);
+
+  const baseCurrency = curr => {
+    setFromCurr(curr);
+  };
+
+  const convertCurrency = curr => {
+    setToCurr(curr);
+  };
 
   return (
     <>
@@ -23,7 +43,7 @@ function App() {
         </option>
         <option value="EUR">EUR</option>
         <option value="GBP">GBP</option>
-        <option value="NIS">NIS</option>
+        <option value="ILS">ILS</option>
         <option value="PHP">PHP</option>
       </select>
       <input
@@ -44,7 +64,7 @@ function App() {
         </option>
         <option value="EUR">EUR</option>
         <option value="GBP">GBP</option>
-        <option value="NIS">NIS</option>
+        <option value="ILS">ILS</option>
         <option value="PHP">PHP</option>
       </select>
       <input
