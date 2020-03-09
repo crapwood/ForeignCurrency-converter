6debc184f849d2ad7e079051188a4864f1dcea00
james <jjbe.93@gmail.com>
2020-03-09 03:46:33 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [result, setResult] = useState(0);
+  const [fromVal, setFromVal] = useState(0);
+  const [toVal, setToVal] = useState(0);
+
+  useEffect(() => {
+    setResult(fromVal * toVal);
+  }, [toVal, fromVal]);
+
+  return (
+    <>
+      <select>
+        <option selected value="USD">
+          USD
+        </option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="NIS">NIS</option>
+        <option value="PHP">PHP</option>
+      </select>
+      <input
+        onChange={e => {
+          setFromVal(e.target.value);
+        }}
+        value={fromVal}
+        type="number"
+      />
+      <strong>TO</strong>
+      <select>
+        <option selected value="USD">
+          USD
+        </option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="NIS">NIS</option>
+        <option value="PHP">PHP</option>
+      </select>
+      <input
+        onChange={e => {
+          setToVal(e.target.value);
+        }}
+        value={toVal}
+        type="number"
+      />
+      <h3>{result}</h3>
+    </>
+  );
+}
+
+export default App;
